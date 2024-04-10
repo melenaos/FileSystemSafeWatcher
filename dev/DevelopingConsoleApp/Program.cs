0ae0ed973c744b55be9b74e9c0b0817666245623
melenaos <menelaosvergis@gmail.com>
2024-04-10 12:58:36 +0300

    Add Filters to support multiple filename filters and created a dev project to test

---
+++ dev/DevelopingConsoleApp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection;
+using Menelabs;
+
+FileSystemSafeWatcher safeWatcher = new FileSystemSafeWatcher("C:\\temp");
+safeWatcher.Filters.Add("*.txt");
+safeWatcher.Filters.Add("*.csv");
